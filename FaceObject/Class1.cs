8355cdb8ad3b9ed1018643a2054cd3d44a99353c
zxy <2932189904@qq.com>
2023-03-01 16:33:13 +0800

    上传一个类文件

---
+++ FaceObject/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceObject
+{
+    class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("这是一个构造函数");
+        }
+    }
+}
